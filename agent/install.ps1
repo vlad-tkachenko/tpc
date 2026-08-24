7951a69f314e4535e57d48faa5375ab140136aff
Vladyslav Tkachenko <vlad@fireblink.com>
2026-08-24 14:41:46 +0300

    chore: installation scripts

--- agent/install.ps1
+++ agent/install.ps1
@@ -1,7 +1,7 @@
 # Start your service application
 cd C:\tpc\agent
 bun install
-pm2 start ecosystem.config.js
+pm2 start ecosystem.config.cjs
 
 # Save the process list so it restores on system reboot
 pm2 save
--- privileged/install.ps1
+++ privileged/install.ps1
@@ -4,7 +4,7 @@ $env:PM2_HOME="C:\ProgramData\pm2"
 # Start your service application
 cd C:\tpc\privileged
 bun install
-pm2 start ecosystem.config.js
+pm2 start ecosystem.config.cjs
 
 # Save the process list so it restores on system reboot
 pm2 save
